227fcc9ec5fc4625694740255e4f601305e27a29
st4rm <armland@gmail.com>
2017-11-27 13:32:40 +0900

    wire data to file every 1 hours

--- DHT_Sensor/ING_dht.py
+++ DHT_Sensor/ING_dht.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import Adafruit_DHT
+import time
+from datetime import datetime
 
 # Sensor should be set to Adafruit_DHT.DHT11, 
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
@@ -14,13 +16,22 @@ pin = 23
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+now=datetime.now()
+filename="{0:%Y}-{0:%m}-{0:%d}".format(now)
+filename +=".txt"
 
+f= open(filename, 'a')
+
+while True:
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-	print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-	print('Failed to get reading. Try again!')
+	if humidity is not None and temperature is not None:
+		data='Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity)
+		f.write(data)
+	else:
+		print('Failed to get reading. Try again!')
+	time.sleep(3600) # write data every 1 hours.
 
+f.clolse()

af29b2fef582108b257bf9756028fc91942ba016
st4rm <33738840+st4rm@users.noreply.github.com>
2017-12-05 09:32:54 +0900

    fix bug

--- DHT_Sensor/DHT_recording.py
+++ DHT_Sensor/DHT_recording.py
@@ -12,29 +12,36 @@ sensor = Adafruit_DHT.DHT11
 # connected to GPIO23.
 pin = 23
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
 now=datetime.now()
 filename="{0:%Y}-{0:%m}-{0:%d}".format(now)
 filename +=".txt"
 
 f= open(filename, 'a')
 
+sec=float(raw_input("Enter the priod of time(second) to recording Temperature and Humidity: "))
+
+print('Temperature and Humidity are recording...')
+
 while True:
+
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
 	if humidity is not None and temperature is not None:
                 now=datetime.now()
 		data="{0:%Y}-{0:%m}-{0:%d} {0:%H}:{0:%M}:{0:%S}".format(now)
 		data += '     Temp={0:0.1f}*C Humidity={1:0.1f}%\n'.format(temperature, humidity)
-                print(data) # print result to standard output(Monitor) 
-		f.write(data) # print result to file
+                print(data)
+		f.write(data)
 	else:
 		print('Failed to get reading. Try again!')
-	time.sleep(600) # write data every 10 minutes.
+	time.sleep(sec) # write data every (sec) Seconds.
 
 f.clolse()
